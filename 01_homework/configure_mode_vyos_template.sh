46be39f0b299c7fd3efb4d94b32185df819200a8
BansheePrime_onPoPOS <cmapuk-xpehob@yandex.ru>
2023-01-15 03:15:49 +0300

    restructuring project folders

--- 01_homework/configure_mode_vyos_template.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Scripts need to be executed as vyos user, not root!
-# https://docs.vyos.io/en/equuleus/automation/command-scripting.html
-
-#!/bin/vbash
-source /opt/vyatta/etc/functions/script-template
-
-exit
